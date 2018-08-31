0d033c9c74c31f293da6318046252d6bc0c19fe4
nohjiho <bestpractice80@gmail.com>
2018-08-31 15:05:31 +0900

    메터 설정 인식 코드 수정

--- cloud_client/transcribe_async.py
+++ cloud_client/transcribe_async.py
@@ -65,9 +65,37 @@ def transcribe_file(speech_file):
     # [END speech_python_migration_async_response]
 # [END speech_transcribe_async]
 
-
 # [START speech_transcribe_async_gcs]
 def transcribe_gcs(gcs_uri):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+    client = speech.SpeechClient()
+
+    audio = types.RecognitionAudio(uri=gcs_uri)
+    config = types.RecognitionConfig(
+        # encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=8000,  # default : 16000
+        language_code='ko-KR'  # 한국어 : ko-KR
+    )
+
+    operation = client.long_running_recognize(config, audio)
+
+    print('Waiting for operation to complete...')
+    response = operation.result(timeout=90)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+        print('Confidence: {}'.format(result.alternatives[0].confidence))
+# [END speech_transcribe_async_gcs]
+
+# [START transcribe_gcs_return]
+def transcribe_gcs_return(gcs_uri):
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
     from google.cloud import speech
     from google.cloud.speech import enums
@@ -105,7 +133,7 @@ def transcribe_gcs(gcs_uri):
     #결과를 빅쿼리에 저장하기 위해 return
     tuple_result_msg = (result_msg, confidence/len(response.results))
     return tuple_result_msg
-# [END speech_transcribe_async_gcs]
+# [END transcribe_gcs_return]
 
 
 if __name__ == '__main__':
@@ -117,5 +145,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     if args.path.startswith('gs://'):
         transcribe_gcs(args.path)
+        #transcribe_gcs_return(gcs_uri)
     else:
         transcribe_file(args.path)
--- test/integration_speech_test.py
+++ test/integration_speech_test.py
@@ -31,86 +31,165 @@ import sys
 import cloud_client.transcribe_async as long_speech
 import cloud_client.save_result_bigquery as save_result_bigquery
 
-# [START speech_transcribe_async_gcs]
+#1. 구글 스토리지의 파일 리스트
+files_gs_path = [(1,'gs://dlab_ml/speech/ref/샘플_1_개인정보삭제.wav'),
+             (2, 'gs://dlab_ml/speech/ref/샘플_2_개인정보삭제.wav'),
+             (3, 'gs://dlab_ml/speech/ref/샘플_3_개인정보삭제.wav'),
+             (4, 'gs://dlab_ml/speech/ref/샘플_4_개인정보삭제.wav'),
+             (5, 'gs://dlab_ml/speech/ref/샘플_5_개인정보삭제.wav'),
+             (6, 'gs://dlab_ml/speech/ref/샘플_6_개인정보삭제.wav'),
+             (7, 'gs://dlab_ml/speech/ref/샘플_7_개인정보삭제.wav'),
+             (8, 'gs://dlab_ml/speech/ref/샘플_8_개인정보삭제.wav'),
+             (9, 'gs://dlab_ml/speech/ref/샘플_9_개인정보삭제.wav'),
+             (10, 'gs://dlab_ml/speech/ref/샘플_10_개인정보삭제.wav')]
+
+# 샘플비율 헤르츠
+sample_rate_hertzs = [(1, 6000),
+                      (2, 8000),
+                      (3, 10000)]
+
+# [START execute]
 def execute():
-    #1. 구글 스토리지의 파일 리스트
-    files_gs_path = [(1,'gs://dlab_ml/speech/ref/샘플_1_개인정보삭제.wav'),
-                     (2, 'gs://dlab_ml/speech/ref/샘플_2_개인정보삭제.wav'),
-                     (3, 'gs://dlab_ml/speech/ref/샘플_3_개인정보삭제.wav'),
-                     (4, 'gs://dlab_ml/speech/ref/샘플_4_개인정보삭제.wav'),
-                     (5, 'gs://dlab_ml/speech/ref/샘플_5_개인정보삭제.wav'),
-                     (6, 'gs://dlab_ml/speech/ref/샘플_6_개인정보삭제.wav'),
-                     (7, 'gs://dlab_ml/speech/ref/샘플_7_개인정보삭제.wav'),
-                     (8, 'gs://dlab_ml/speech/ref/샘플_8_개인정보삭제.wav'),
-                     (9, 'gs://dlab_ml/speech/ref/샘플_9_개인정보삭제.wav'),
-                     (10, 'gs://dlab_ml/speech/ref/샘플_10_개인정보삭제.wav')]
-
-    # 샘플비율 헤르츠
-    sample_rate_hertzs = [(1, 6000),
-                     (2, 8000),
-                     (3, 10000)]
-
-    #오디오 사용 사례
-    interactionTypes = [(1, 'INTERACTION_TYPE_UNSPECIFIED'),
-                          (2, 'DISCUSSION'),
-                          (3, 'PRESENTATION')]
-    #오디오 산업 카테고리
-    industryNaicsCodeOfAudios = [(1, '522291'),
-                          (3, 'PRESENTATION')]
-
-    #스피커에서 마이크 까지의 거리
-    microphoneDistances =  [(1, 'MICROPHONE_DISTANCE_UNSPECIFIED'),
-                          (2, 'NEARFIELD'),
-                          (3, 'MIDFIELD')]
-
-    # 오디오의 원본 오디오 또는 비디오 중 하나
-    originalMediaTypes = [(1, 'ORIGINAL_MEDIA_TYPE_UNSPECIFIED'),
-                           (2, 'AUDIO')]
-
-    #오디오 캡쳐 장치 종류
-    recordingDeviceTypes = [(1, 'RECORDING_DEVICE_TYPE_UNSPECIFIED'),
-                          (2, 'SMARTPHONE'),
-                          (3, 'PHONE_LINE')]
-
     # 2. 파일 수 만큼 loop를 돌려 긴오디오 인식을 수행한다.  (메터 데이터 인식 제외)
-    for (speech_num, file_gs_path) in files_gs_path:
-        print(' ============ start ', file_gs_path, ' ===============')
-        tuple_result_msg = ()
-        result_msg = ''
-        except_msg = ''
-        confidence = 0
-        stt_status = 1
-        try:
-            #2-2. 긴 오디오 파일 인식 결과 가져오기
-            tuple_result_msg = long_speech.transcribe_gcs(file_gs_path)
-            result_msg = tuple_result_msg[0]
-            confidence = tuple_result_msg[1]
-        except:
-            stt_status = -1
-            #TODO : 오류가 발생하면 except_msg 변수에 담는다.
-            except_msg = sys.exc_info()
-            print("Unexpected error:", except_msg)
-            #오류 무시
-            pass
-        finally:
-            print('result_msg : ', result_msg)
-            #2-3.빅쿼리에 결과 저장
-            try:
-                save_result_bigquery.saveBigQuery(speech_num, '2', stt_status, except_msg, result_msg,
-                     8000, '', False, 'DISCUSSION', '522291',
-                     'NEARFIELD', 'AUDIO', 'PHONE_LINE', 100, '1:30',
-                      confidence)
-            except:
-                except_msg = sys.exc_info()
-                print("Unexpected error:", except_msg)
-                pass
-            print(' ============ end ', file_gs_path, ' ===============')
+    excuteLongAudio()
 
     #3. 파일 수 만큼 loop를 돌려 긴오디오 인식을 수행한다.(메터 데이터 설정 인식)
+    executeMetaLongAudio()
+# [END execute]
 
+#파일 수 만큼 loop를 돌려 긴오디오 인식을 수행한다.  (메터 데이터 인식 제외)
+def excuteLongAudio():
+    for (speech_num, file_gs_path) in files_gs_path:
+        for sample_rate_hertz in sample_rate_hertzs:
+            print(' ============ start ', file_gs_path, ', ', sample_rate_hertz, ' ===============')
+            tuple_result_msg = ()
+            result_msg = ''
+            except_msg = ''
+            confidence = 0
+            stt_status = 1
+            try:
+                #2-2. 긴 오디오 파일 인식 결과 가져오기
+                tuple_result_msg = long_speech.transcribe_gcs_return(file_gs_path)
+                result_msg = tuple_result_msg[0]
+                confidence = tuple_result_msg[1]
+            except:
+                stt_status = -1
+                #TODO : 오류가 발생하면 except_msg 변수에 담는다.
+                except_msg = sys.exc_info()
+                print("Unexpected error:", except_msg)
+                #오류 무시
+                pass
+            finally:
+                print('result_msg : ', result_msg)
+                #2-3.빅쿼리에 결과 저장
+                try:
+                    save_result_bigquery.saveBigQuery(speech_num, '2', stt_status, except_msg, result_msg,
+                                                      sample_rate_hertz, '', False, 'DISCUSSION', '522291',
+                                                      'NEARFIELD', 'AUDIO', 'PHONE_LINE', 100, '1:30',
+                                                      confidence)
+                except:
+                    except_msg = sys.exc_info()
+                    print("Unexpected error:", except_msg)
+                    pass
 
+                    print(' ============ end ', file_gs_path, ', ', sample_rate_hertz, ' ===============')
 
-# [END speech_transcribe_async_gcs]
+def executeMetaLongAudio():
+    from google.cloud import speech_v1p1beta1 as speech
+    # 오디오 사용 사례
+    #INTERACTION_TYPE_UNSPECIFIED : 사용 사례가 알려지지 않았거나 아래의 다른 값 중 하나 이외의 값
+    #DISCUSSION : 대화  또는 토론에 참여한 여러 사람
+    #PRESENTATION : 하나 또는 그 이상의 사람들이 강의
+    interactionTypes = [(1, speech.enums.RecognitionMetadata.InteractionType.INTERACTION_TYPE_UNSPECIFIED),
+                        (2, speech.enums.RecognitionMetadata.InteractionType.DISCUSSION),
+                        (3, speech.enums.RecognitionMetadata.InteractionType.PRESENTATION)]
+    # 오디오 산업 카테고리
+    #522291 : 소비자 금융 회사 (즉, 무담보 현금 대출)
+    #522220 : 자동차 금융리스 기업
+    industryNaicsCodeOfAudios = [(1, 522291),
+                                 (2, 522220)]
+
+    # 스피커에서 마이크 까지의 거리
+    #MICROPHONE_DISTANCE_UNSPECIFIED : 오디오 유형을 알 수 없습니다.
+    #NEARFIELD : 오디오는 밀접하게 배치 된 마이크에서 캡처했습니다 (전화, 마이크) 1미터이내
+    #MIDFIELD : 스피커가 마이크에서 3 미터 이내
+    microphoneDistances = [(1, speech.enums.RecognitionMetadata.microphoneDistances.MICROPHONE_DISTANCE_UNSPECIFIED),
+                           (2, speech.enums.RecognitionMetadata.microphoneDistances.NEARFIELD),
+                           (3, speech.enums.RecognitionMetadata.microphoneDistances.MIDFIELD)]
+
+    # 오디오의 원본 오디오 또는 비디오 중 하나
+    #ORIGINAL_MEDIA_TYPE_UNSPECIFIED : 알 수없는 원본 미디어 유형입니다.
+    #AUDIO : 음성 데이터는 오디오 녹음입니다.
+    originalMediaTypes = [(1, speech.enums.RecognitionMetadata.originalMediaTypes.ORIGINAL_MEDIA_TYPE_UNSPECIFIED),
+                          (2, speech.enums.RecognitionMetadata.originalMediaTypes.AUDIO)]
+
+    # 오디오 캡쳐 장치 종류
+    #RECORDING_DEVICE_TYPE_UNSPECIFIED : 녹음 장치를 알 수 없습니다.
+    #SMARTPHONE : 음성은 스마트 폰에 녹음되었습니다.
+    #PHONE_LINE : 연설은 전화선을 통해 기록되었습니다.
+    recordingDeviceTypes = [(1, speech.enums.RecognitionMetadata.recordingDeviceTypes.RECORDING_DEVICE_TYPE_UNSPECIFIED),
+                            (2, speech.enums.RecognitionMetadata.recordingDeviceTypes.SMARTPHONE),
+                            (3, speech.enums.RecognitionMetadata.recordingDeviceTypes.PHONE_LINE)]
+    for (speech_num, file_gs_path) in files_gs_path:
+        for sample_rate_hertz in sample_rate_hertzs:
+            for interactionType in interactionTypes:
+                for industryNaicsCodeOfAudio in industryNaicsCodeOfAudios:
+                    for microphoneDistance in microphoneDistances:
+                        for originalMediaType in originalMediaTypes:
+                            for recordingDeviceType in recordingDeviceTypes:
+                                print(' ============ START file_gs_path :', file_gs_path,
+                                      ', sample_rate_hertz : ', sample_rate_hertz,
+                                      ', interactionType : ', interactionType,
+                                      ', industryNaicsCodeOfAudio : ', industryNaicsCodeOfAudio,
+                                      ', microphoneDistance : ', microphoneDistance,
+                                      ', originalMediaType : ', originalMediaType,
+                                      ', recordingDeviceType : ', recordingDeviceType,
+                                      ' ===============')
+                                tuple_result_msg = ()
+                                result_msg = ''
+                                except_msg = ''
+                                confidence = 0
+                                stt_status = 1
+                                try:
+                                    #2-2. 긴 오디오 파일 인식 결과 가져오기
+                                    tuple_result_msg = long_speech.transcribe_gcs_with_metadata_return(file_gs_path,                    #스토리지 uri
+                                                                                                        sample_rate_hertz,         #샘플비율 헤르츠
+                                                                                                        interactionType,           #오디오 사용 사례
+                                                                                                        industryNaicsCodeOfAudio,  #오디오 산업 카테고리
+                                                                                                        microphoneDistance,        #스피커에서 마이크 까지의 거리
+                                                                                                        originalMediaType,         #오디오의 원본 오디오 또는 비디오 중 하나
+                                                                                                        recordingDeviceType)
+
+                                    result_msg = tuple_result_msg[0]
+                                    confidence = tuple_result_msg[1]
+                                except:
+                                    stt_status = -1
+                                    #TODO : 오류가 발생하면 except_msg 변수에 담는다.
+                                    except_msg = sys.exc_info()
+                                    print("Unexpected error:", except_msg)
+                                    #오류 무시
+                                    pass
+                                finally:
+                                    print('result_msg : ', result_msg)
+                                    #2-3.빅쿼리에 결과 저장
+                                    try:
+                                        save_result_bigquery.saveBigQuery(speech_num, '2', stt_status, except_msg, result_msg,
+                                                                          sample_rate_hertz, '', False, 'DISCUSSION', '522291',
+                                                                          'NEARFIELD', 'AUDIO', 'PHONE_LINE', 100, '1:30',
+                                                                          confidence)
+                                    except:
+                                        except_msg = sys.exc_info()
+                                        print("Unexpected error:", except_msg)
+                                        pass
+
+                                        print(' ============ END file_gs_path :', file_gs_path,
+                                              ', sample_rate_hertz : ', sample_rate_hertz,
+                                              ', interactionType : ', interactionType,
+                                              ', industryNaicsCodeOfAudio : ', industryNaicsCodeOfAudio,
+                                              ', microphoneDistance : ', microphoneDistance,
+                                              ', originalMediaType : ', originalMediaType,
+                                              ', recordingDeviceType : ', recordingDeviceType,
+                                              ' ===============')
 
 if __name__ == '__main__':
     execute()
